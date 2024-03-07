a11b3a4248cea0dfd6a5a9b636a5e1dfeb9028b6
Matthew Darling <matthewjdarling@gmail.com>
2024-03-07 11:45:58 -0500

    Add basic Reitit + Ring API server
    State handling should be modernized, it's currently very 2014 for the
    server starting and stopping. Probably also need to start using Reitit's
    conversion features to improve parameter handling. And maybe hopefully
    figure out why I need an async handler.

--- src/app/server.clj
+++ src/app/server.clj
@@ -1,6 +1,11 @@
 (ns app.server
   (:require [honey.sql :as sql]
-            [next.jdbc :as jdbc])
+            [next.jdbc :as jdbc]
+            [reitit.http :as http]
+            [reitit.ring :as ring]
+            [reitit.ring.middleware.parameters :as middleware]
+            [ring.adapter.jetty :as jetty]
+            [ring.util.response :as response])
   (:gen-class))
 
 (sql/register-op! (keyword "@@"))
@@ -35,3 +40,69 @@
                :headline]]
      :from [:files]
      :where [(keyword "@@") :content [(query-mode->query-fn query-mode) query]]}))
+
+(defn retrieve-file-handler
+  "Async and sync compatible handler. Most example online show only a synchronous
+  handler, but when I only define the one arg version, I get 'wrong number of
+  args' when calling with curl."
+  ([{{:strs [file_id]}
+     :query-params :as request}]
+   (println request)
+   (let [file-id (Integer/parseInt file_id)
+         my-datasource (jdbc/get-datasource db-config)]
+     (with-open [connection (jdbc/get-connection my-datasource)]
+       (response/response
+        (pr-str (jdbc/execute! connection
+                               (sql/format {:select [:filename :title :author :content]
+                                            :from   [:files]
+                                            :where  [:= :files.file_id file-id]})))))))
+  ([request respond raise]
+   (respond (retrieve-file-handler request))))
+
+(def router
+  (ring/router
+    ["/api"
+     ["/file" {:get retrieve-file-handler}]
+     ["/list" {:get (fn [{{:strs [query query_mode strategy]}
+                          :query-params :as req}]
+                      (let [my-datasource (jdbc/get-datasource db-config)]
+                        (with-open [connection (jdbc/get-connection my-datasource)]
+                          (jdbc/execute! connection
+                                         (sql/format (compile-search-query query
+                                                                           query_mode
+                                                                           strategy))))))}]]))
+
+(def app
+  (ring/ring-handler
+   router
+   (ring/create-default-handler
+    {:not-found (constantly
+                 {:status 404
+                  :body "Not found"})})
+   {:middleware [middleware/parameters-middleware]}))
+
+(def port 3000)
+
+(defonce server-var nil)
+
+(defn start [_]
+  (let [server (jetty/run-jetty #'app {:port port, :join? false, :async? true})]
+    (println "server running in port" port)
+    server))
+
+(defn stop []
+  (.stop server-var)
+  (alter-var-root #'server-var (constantly nil)))
+
+(defn -main [& args]
+  (alter-var-root #'server-var start))
+
+(defn reset []
+  (stop)
+  (-main))
+
+(comment
+  (-main)
+  (app {:request-method :get, :uri "/api/file?file_id=26"})
+  (stop)
+  (reset))
