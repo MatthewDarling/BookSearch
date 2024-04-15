7ab9d44bbdea388df6d931844211bf8e4a1f2fe5
Matthew Darling <matthewjdarling@gmail.com>
2024-04-15 16:33:22 -0400

    Use Flyway to install the custom PG extensions

--- sql/booksearch/V003__create_schema.sql
+++ sql/booksearch/V003__create_schema.sql
@@ -1,3 +1,5 @@
+CREATE EXTENSION unaccent;
+
 CREATE TABLE IF NOT EXISTS files (file_id SERIAL PRIMARY KEY,
                                   filename TEXT NOT NULL,
                                   title TEXT NOT NULL,
